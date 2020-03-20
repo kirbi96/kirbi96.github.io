a042aea41f3c8d125bca43599164341f16f0bdef
kirbi96 <58092256+kirbi96@users.noreply.github.com>
2020-03-21 02:07:17 +0300

    version-1
    Для просмотра сайта перейти по ссылке kirbi96.github.io/stalker/

---
+++ stalker/js/script.js
@@ -0,0 +1,12 @@
+//mobile__menu
+let countMob = 0;
+function toggleStyle(){
+    ++countMob;
+    var mob = document.getElementById("mobile__menu");
+    if ( countMob % 2 !== 0){
+        mob.classList.remove('mobile__no');
+    }
+    else {
+        mob.classList.add('mobile__no');
+    }
+}
